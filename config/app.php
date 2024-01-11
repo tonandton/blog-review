38d8bfa91371935e23097de49e703584e574d953
tonandton <www.sorasakok@gmail.com>
2024-01-12 06:38:38 +0700

    create database

--- config/app.php
+++ config/app.php
@@ -70,7 +70,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // 'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
@@ -156,16 +157,16 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
+            /*
+             * Package Service Providers...
+             */
 
-        /*
-         * Application Service Providers...
-         */
+            /*
+             * Application Service Providers...
+             */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+            // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,33 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numberReviews = random_int(5, 30);
+
+            Review::factory()->count($numberReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numberReviews = random_int(5, 30);
+
+            Review::factory()->count($numberReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numberReviews = random_int(5, 30);
+
+            Review::factory()->count($numberReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
+
 }
