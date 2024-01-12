0d5dc97993645b3ec5d5e77533ffe2c95c94ea18
mesuk <it@me-suk.com>
2024-01-12 16:52:45 +0700

    ..

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Contracts\Database\Query\Builder;
 
 class Book extends Model
 {
@@ -12,5 +14,19 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder {
+        // return $query->withCount(['reviews' =>])
+        // ->orderBy('reviews_count', 'desc');
+    }    
+
+    public function scopeHighestRated(Builder $query): Builder | QueryBuilder {
+        return $query->withAvg('reviews', 'rating')
+        ->orderBy('reviews_avg_rating', 'desc');
     }
 }
