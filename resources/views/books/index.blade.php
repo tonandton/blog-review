e0deace9819d2786eb3c537a7a5aab6df1640f6a
tonandton <www.sorasakok@gmail.com>
2024-01-15 21:50:26 +0700

    update review rate

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2x1">Books</h1>
+    <h1 class="mb-10 text-2x1">App Reviews</h1>
 
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
         <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input h-10">
@@ -40,7 +40,7 @@
                         <div>
                             <div class="book-rating">
                                 {{-- {{ number_format($book->reviews_avg_rating, 1) }} --}}
-                                <x-star-rating :rating="$book->reviews_avg_rating"/>
+                                <x-star-rating :rating="$book->reviews_avg_rating" />
                             </div>
                             <div class="book-review-count">
                                 out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -9,13 +9,20 @@
             <div class="book-rating flex items-center">
                 <div class="mr-2 text-sm font-medium text-slate-700">
                     {{-- {{ number_format($book->reviews_avg_rating, 1) }} --}}
-                    <x-star-rating :rating="$book->reviews_avg_rating"/>
+                    <x-star-rating :rating="$book->reviews_avg_rating" />
                 </div>
                 <span class="book-review-count text-sm text-gray-500">
                     {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
                 </span>
             </div>
         </div>
+    </div>
+
+    <div class="mb-4">
+        <a href="{{ route('books.index') }}" class="text-rose-500 ">← Back</a>
+        <a href="{{ route('books.reviews.create', $book) }}" class="reset-link float-right">
+            Add a review!
+        </a>
     </div>
 
     <div>
@@ -25,7 +32,7 @@
                 <li class="book-item mb-4">
                     <div>
                         <div class="mb-2 flex items-center justify-between">
-                            <div class="font-semibold">  <x-star-rating :rating="$review->rating"/></div>
+                            <div class="font-semibold"> <x-star-rating :rating="$review->rating" /></div>
                             <div class="book-review-count">
                                 {{ $review->created_at->format('M j, Y') }}</div>
                         </div>
